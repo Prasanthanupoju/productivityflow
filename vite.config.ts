4fc90820871d01142ad265417f1d84882d7fe7b5
Prasanthanupoju <149069446+Prasanthanupoju@users.noreply.github.com>
2025-08-13 00:04:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // ✅ This fixes CSS/assets not loading on Render
   server: {
     host: "::",
     port: 8080,
